c876a1b7fe527dc67141eaa884f2be05804ce5b8
tiyarocks <51966091+tiyarocks@users.noreply.github.com>
2020-04-06 19:06:04 +0530

    Add files via upload
    handling async axios requests via async await

--- src/components/App.js
+++ src/components/App.js
@@ -3,17 +3,16 @@ import axios from "axios"
 import SearchBar from "./SearchBar"
 class App extends React.Component{
 
-    onSearchSubmit(term){
-        axios.get("https://api.unsplash.com/search/photos",{
+    async onSearchSubmit(term){
+        const response=await axios.get("https://api.unsplash.com/search/photos",{
             params:{
                 query:term
             },
             headers:{
                 Authorization:"Client-ID J1OJ3fQlRKFezu-1e_yXiIBpZ3QgX5vMXo5iQUAFM8w"
             }
-        }).then((response)=>{
-            console.log(response.data.results)
         })
+        console.log(response.data.results)
     }
     render(){
         return (    
