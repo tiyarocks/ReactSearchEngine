4b909ec23b560c51117b576feb021e44f7deef8e
tiyarocks <51966091+tiyarocks@users.noreply.github.com>
2020-04-06 18:59:30 +0530

    Add files via upload
    handling axios requests with promises

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,19 @@
 import React from "react"
+import axios from "axios"
 import SearchBar from "./SearchBar"
 class App extends React.Component{
 
     onSearchSubmit(term){
-        console.log(term)
+        axios.get("https://api.unsplash.com/search/photos",{
+            params:{
+                query:term
+            },
+            headers:{
+                Authorization:"Client-ID J1OJ3fQlRKFezu-1e_yXiIBpZ3QgX5vMXo5iQUAFM8w"
+            }
+        }).then((response)=>{
+            console.log(response.data.results)
+        })
     }
     render(){
         return (    
